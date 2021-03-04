325fe02a324fdca700a97c89b53d913ed0299b2a
james <jamesearl341@gmail.com>
2021-03-04 16:57:50 +0000

    updates

--- app/tests/core/controllers/test_spotify.py
+++ app/tests/core/controllers/test_spotify.py
@@ -2,27 +2,27 @@ TEST_CONNECTED_CLIENTS_IP = '127.0.0.255'
 
 
 def test_spotify_init():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
 
     assert spotify is not None
 
 def test_spotify_single_instance():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify_instance_1 = Spotify.Instance()
     spotify_instance_2 = Spotify.Instance()
 
     assert spotify_instance_1 == spotify_instance_2
 
 def test_generate_random_state():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
 
     state = spotify._generate_random_state()
     assert state
 
 def test_save_generated_random_state():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
     random_state = spotify._generate_random_state()
 
@@ -30,7 +30,7 @@ def test_save_generated_random_state():
     assert saved_state == random_state
 
 def test_check_spotify_state_exists():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
     random_state = spotify._generate_random_state()
         
@@ -39,7 +39,7 @@ def test_check_spotify_state_exists():
     assert saved_state == random_state
  
 def test_get_callback_url_web_request_to_spotify():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
 
     url = "https://accounts.spotify.com/authorize"
@@ -49,7 +49,7 @@ def test_get_callback_url_web_request_to_spotify():
     print(response_url)
 
 def test_get_callback_url():
-    from core.controllers.spotify import Spotify
+    from app.core.controllers.spotify import Spotify
     spotify = Spotify.Instance()
 
     redirect_url = spotify.get_callback_url(TEST_CONNECTED_CLIENTS_IP)
--- app/tests/core/controllers/test_spotify_songs.py
+++ app/tests/core/controllers/test_spotify_songs.py
@@ -1,10 +1,10 @@
 import sys
 
 def test_spotify_songs_init():
-    from core.controllers.spotify_songs import Spotify_songs
+    from app.core.controllers.spotify_songs import Spotify_songs
     spotify_songs = Spotify_songs.Instance()
 
-    assert spotify is not None
+    assert spotify_songs is not None
 
 def test_get_chords_url_from_google_search_api():
     from app.core.controllers.spotify_songs import Spotify_songs
